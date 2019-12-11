38bcef339c7099b901d7c8a7a31224ed549bffc9
Levi Lunique <levi.lunique@gmail.com>
2019-12-11 00:07:38 -0300

    Platform and variable jump

--- Endless Runner Project/Assets/Scripts/Controller2D.cs
+++ Endless Runner Project/Assets/Scripts/Controller2D.cs
@@ -2,47 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent (typeof (BoxCollider2D))]
-public class Controller2D : MonoBehaviour
+
+public class Controller2D : RaycastController
 {
-    public LayerMask collisionMask;
-    const float skinWidth = .015f;
-    public int horizontalRayCount = 4;
-    public int verticalRayCount = 4;
 
     float maxClimbAngle = 80;
     float maxDescendAngle = 80;
 
-    float horizontalRaySpacing;
-    float verticalRaySpacing;
-
-    BoxCollider2D collider;
-    RaycastOrigins raycastOrigins;
     public CollisionInfo collisions;
+    Vector2 playerInput;
     
-    void Start() 
+    public override void Start() 
     {
-        collider = GetComponent<BoxCollider2D>();
-        CalculateRaySpacing();
+        base.Start();
         collisions.faceDir = 1;
     }
 
-    public void Move(Vector3 velocity) 
+    public void Move(Vector3 velocity, bool standingOnPlatform = false) 
+    {
+        Move(velocity, Vector2.zero, standingOnPlatform);
+    }
+
+    public void Move(Vector3 velocity, Vector2 input, bool standingOnPlatform = false) 
     {
         UpdateRaycastOrigins();
         collisions.Reset();
         collisions.velocityOld = velocity;
+        playerInput = input;
 
         if (velocity.x != 0) 
         {
             collisions.faceDir = (int)Mathf.Sign(velocity.x);
         }
-
+        
         if (velocity.y < 0) 
         {
             DescendSlope(ref velocity);
         }
-
+        
         HorizontalCollisions(ref velocity);
         
         if (velocity.y != 0) 
@@ -52,6 +49,11 @@ public class Controller2D : MonoBehaviour
         
 
         transform.Translate(velocity);
+
+        if (standingOnPlatform) 
+        {
+            collisions.below = true;
+        }
     }
 
     void HorizontalCollisions(ref Vector3 velocity) 
@@ -74,6 +76,10 @@ public class Controller2D : MonoBehaviour
 
             if (hit) 
             {
+                if (hit.distance == 0) 
+                {
+                    continue;
+                }
                 float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
                 
                 if (i == 0 && slopeAngle <= maxClimbAngle) 
@@ -126,6 +132,23 @@ public class Controller2D : MonoBehaviour
 
             if (hit)
             {
+                if (hit.collider.tag == "Through") 
+                {
+                    if (directionY == 1 || hit.distance == 0) 
+                    {
+                        continue;
+                    }
+                    if (collisions.fallingThrougPlatform) 
+                    {
+                        continue;
+                    }
+                    if (playerInput.y == -1) 
+                    {
+                        collisions.fallingThrougPlatform = true;
+                        Invoke("ResetFallingThroughPlatform", .5f);
+                        continue;
+                    }
+                }
                 velocity.y = (hit.distance - skinWidth) * directionY;
                 rayLength = hit.distance;
 
@@ -205,34 +228,9 @@ public class Controller2D : MonoBehaviour
         }
     }
 
-    void UpdateRaycastOrigins() 
+    void ResetFallingThroughPlatform ()
     {
-        Bounds bounds = collider.bounds;
-        bounds.Expand(skinWidth * -2);
-
-        raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
-        raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
-        raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
-        raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
-    }
-
-    void CalculateRaySpacing() 
-    {
-        Bounds bounds = collider.bounds;
-        bounds.Expand(skinWidth * -2);
-
-        horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
-        verticalRayCount = Mathf.Clamp(verticalRayCount, 2, int.MaxValue);
-
-        horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
-        verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
-
-    }
-
-    struct RaycastOrigins 
-    {
-        public Vector2 topLeft, topRight;
-        public Vector2 bottomLeft, bottomRight;
+        collisions.fallingThrougPlatform = false;
     }
 
     public struct CollisionInfo 
@@ -245,6 +243,7 @@ public class Controller2D : MonoBehaviour
         public float slopeAngle, slopeAngleOld;
         public Vector3 velocityOld;
         public int faceDir;
+        public bool fallingThrougPlatform;
 
         public void Reset() 
         {
